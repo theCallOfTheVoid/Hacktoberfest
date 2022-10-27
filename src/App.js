6e12db0c8b3b548046aef64bb77bba13a32fcb9c
abhaysood1793 <abhayasood99@gmail.com>
2022-10-27 12:17:41 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="row">
-      <div className='col-12'>Hello </div>
-      <div className='col-12'>World</div>
+      <div className='col-6'>Hello </div>
+      <div className='col-6'>World</div>
     </div>
   );
 }
