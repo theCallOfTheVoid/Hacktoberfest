d32fb5f36df68d3be1fad56be1b346a11c69340a
abhaysood1793 <abhayasood99@gmail.com>
2022-10-27 12:07:54 +0530

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="row">
+      <div className='col-12'>Hello </div>
+      <div className='col-12'>World</div>
+    </div>
+  );
+}
+
+export default App;
